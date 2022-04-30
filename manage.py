be9a02a454604c5650b7f6ed5e542108b3805835
Robert Kirui <kirugik79@gmail.com>
2022-04-30 13:27:08 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -12,6 +12,13 @@ manager = Manager(app)
 # Then, use the add_command method to create a new command 'server' which will launch the app server 
 manager.add_command('server',Server) 
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests) 
+
 # Calling the run method on the Manager instance(manager) to run the application 
 if __name__ == '__main__': 
     manager.run() 
